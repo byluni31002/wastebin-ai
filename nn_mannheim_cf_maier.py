c35f69fce1e48995abbe6926eff4ca2efff2cb40
Lukas Nils Ilies <ililuc17@htl-kaindorf.at>
2021-07-14 11:59:26 +0000

    Replace nn_mannheim_cf_maier.py

--- nn_mannheim_cf_maier.py
+++ nn_mannheim_cf_maier.py
@@ -3,8 +3,8 @@ import pandas as pd
 import tensorflow as tf
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-import PySimpleGUI as sg
 import numpy as np
+import get_csv_data
 
 echos = []
 fuellstand = []
@@ -35,7 +35,7 @@ def file_input():
 
 def neuronales_netz_testdaten_implementierne():
     file_input()
-    model.fit(echos, fuellstand, epochs=85)
+    model.fit(echos, fuellstand, epochs=75)
     test_loss, test_acc = model.evaluate(echos, fuellstand, verbose=2)
     print('Test accuracy:', test_acc)
 
@@ -52,31 +52,88 @@ def string2int(str):
     float_array_2d.append(intarray)
     return float_array_2d
 
-def demo_gui():
-    layout = [[sg.InputText(), sg.Text("Echo")],
-              [sg.Button("OK")],
-              [sg.Button("EXIT")]]
-    window = sg.Window("Demo", layout)
-
-    while True:
-        event, values = window.read()
-        if event == "OK":
-            p = predict(string2int(values[0]))
-            sg.popup('Ist: ', class_names[np.argmax(p)])
-        if event == "EXIT" or event == sg.WIN_CLOSED:
-            break
-    window.close()
+def get_dump(dump1,dump2):
+    d1 = dump1.split(',')
+    d2 = dump2.split(',')
+    dump = []
+    for d in d1:
+        if(d != ''):
+            dump.append(d)
+    for d in d2:
+        if (d != ''):
+            dump.append(d)
+    output = ''
+    i = 0
+    while(i<len(dump)):
+        if(i==0):
+            output = output + dump[i]
+        else:
+            output = output + str(', ' + dump[i])
+        i += 1
+    return output
 
 if __name__ == "__main__":
+    print('input database csv')
+    print(get_csv_data.data_file_input())
     model = keras.Sequential([tf.compat.v1.keras.layers.Flatten(input_shape=(0, input_layer_nodes)),
                               tf.compat.v1.keras.layers.Dense(hidden_layer_1_nodes, activation=tf.nn.relu),
                               tf.compat.v1.keras.layers.Dense(hidden_layer_2_nodes, activation=tf.nn.relu),
                               tf.compat.v1.keras.layers.Dense(output_layer_nodes, activation=tf.nn.softmax),
                               ])
-
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
-
+    print('input train data for ai')
     neuronales_netz_testdaten_implementierne()
-    demo_gui()
+    print('1: input mac address - 2: input ptofilename')
+    inp = input()
+    if(inp == '1'):
+        print('enter mac:')
+        inp = input()
+        dump1 = ''
+        dump2 = ''
+        for data in get_csv_data.get_by_mac(inp):
+            if (data[4] == 'Fill Level'):
+                print('Given fill level: ', data[5])
+                dump2 = ''
+            elif (data[4] == 'Echo Dump Part 1'):
+                dump1 = data[5]
+            elif (data[4] == 'Echo Dump Part 2'):
+                dump2 = data[5]
+            else:
+                dump1 = ''
+                dump2 = ''
+
+            if (dump1 != '' and dump2 != ''):
+                dump = get_dump(dump1, dump2)
+                p = predict(string2int(dump))
+                print('Echo dump: ', dump)
+                print('predicted fill level: ', class_names[np.argmax(p)], '\n')
+        print('\n')
+        pass
+    elif(inp == '2'):
+        print('enter profilename:')
+        inp = input()
+        macs = get_csv_data.get_macs_from_profilename(inp)
+        for mac in macs:
+            print('mac: ', mac)
+            dump1 = ''
+            dump2 = ''
+            for data in get_csv_data.get_by_mac(mac):
+                if (data[4] == 'Fill Level'):
+                    print('Given fill level: ', data[5])
+                    dump2 = ''
+                elif (data[4] == 'Echo Dump Part 1'):
+                    dump1 = data[5]
+                elif (data[4] == 'Echo Dump Part 2'):
+                    dump2 = data[5]
+                else:
+                    dump1 = ''
+                    dump2 = ''
+
+                if (dump1 != '' and dump2 != ''):
+                    dump = get_dump(dump1, dump2)
+                    p = predict(string2int(dump))
+                    print('Echo dump: ', dump)
+                    print('predicted fill level: ', class_names[np.argmax(p)], '\n')
+        print('\n')
